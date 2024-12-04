6f942aaa607aebaf0064b6042ec33de6a1a25874
kazuto1314 <kazuto.swimmer.1314@gmail.com>
2024-12-04 22:35:54 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-const today = new Date(2024, 9, 12);
-
-const year = today.getFullYear();  
-const month = today.getMonth() + 1;  
-const date = today.getDate();  
-
-console.log(`${year}年${month}月${date}日`);
